9dea3d792f3d411e417d5f7e149dac3f1609f5ca
“Elham” <alham@gmail.com>
2026-07-21 14:23:44 +0400

    Add Subscription DTOs for creating and displaying subscriptions

--- FitnessPlatform/FitnessPlatform/DTOs/SubscriptionDTOs.cs
+++ FitnessPlatform/FitnessPlatform/DTOs/SubscriptionDTOs.cs
@@ -1,6 +1,72 @@
-﻿namespace FitnessPlatform.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitnessPlatform.DTOs
 {
     public class SubscriptionDTOs
     {
+        // =====================================
+        // Used when creating a new subscription
+        // =====================================
+        public class SubscriptionInputDTO
+        {
+            [Required(ErrorMessage = "Member is required.")]
+            public int memberId { get; set; }
+
+
+            [Required(ErrorMessage = "Membership plan is required.")]
+            public int planId { get; set; }
+        }
+
+        // =====================================
+        // Used when displaying all subscriptions
+        // =====================================
+        public class SubscriptionOutputDTO
+        {
+            public int subscriptionId { get; set; }
+
+
+            public string fullName { get; set; }
+
+
+            public string planName { get; set; }
+
+
+            public bool IsActive { get; set; }
+        }
+
+        // =====================================
+        // Used when displaying subscription details
+        // =====================================
+        public class SubscriptionDetailsDTO
+        {
+            public int subscriptionId { get; set; }
+
+
+
+            public string fullName { get; set; }
+
+
+
+            public string planName { get; set; }
+
+
+
+            public decimal price { get; set; }
+
+
+
+            public int durationInDays { get; set; }
+
+
+
+            public DateTime startDate { get; set; }
+
+
+            public DateTime EndDate { get; set; }
+
+
+
+            public bool IsActive { get; set; }
+        }
     }
 }
