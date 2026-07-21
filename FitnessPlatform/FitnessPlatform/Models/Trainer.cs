71596a74f0be643e02432293bdd9ddecd0db0754
kareema <kareema27-19@gmail.com>
2026-07-21 10:13:35 +0400

    Implement Trainer model

--- FitnessPlatform/FitnessPlatform/Models/Trainer.cs
+++ FitnessPlatform/FitnessPlatform/Models/Trainer.cs
@@ -1,6 +1,52 @@
-﻿namespace FitnessPlatform.Models
+﻿
+
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FitnessPlatform.Models
 {
+    [Table("Trainers")]
     public class Trainer
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int traninerId { get; set; }  // Primary Key system generated
+
+        [Required(ErrorMessage = "Full name is required.")]
+        [MaxLength(100)]
+        public string fullName { get; set; } // user input
+
+        [Required(ErrorMessage = "Phone number is required.")]
+        [MaxLength(15)]
+        public string phoneNumber { get; set; } // user input
+
+        [Required(ErrorMessage = "specialization is required.")]
+        [MaxLength(100)]
+        public string specialization { get; set; } // Entered by the trainer
+
+        [Required(ErrorMessage = "yearsOfExperience is required.")]
+        [Range(0, 50)]
+        public int yearsOfExperience { get; set; } // user input
+
+        [MaxLength(200)]
+        public string? certification { get; set; } // user input
+
+        // Navigation Properties
+        public User User { get; set; }// One User has one Trainer profile
+
+        public List<WorkoutProgram>? WorkoutPrograms { get; set; } // One Trainer can create many workout programs
+
+        public List<NutritionPlan>? NutritionPlans { get; set; }     // One Trainer can create many nutrition plans
+
+
+
+
+
+
+
+
+
     }
 }
