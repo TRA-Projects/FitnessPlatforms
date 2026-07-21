88b8ce48d685219eb4ebb0391bf0374fc82c1c48
kareema <kareema27-19@gmail.com>
2026-07-21 10:44:24 +0400

    Add User foreign key to Trainer model

--- FitnessPlatform/FitnessPlatform/Models/Trainer.cs
+++ FitnessPlatform/FitnessPlatform/Models/Trainer.cs
@@ -14,24 +14,35 @@ namespace FitnessPlatform.Models
 
         public int traninerId { get; set; }  // Primary Key system generated
 
+
         [Required(ErrorMessage = "Full name is required.")]
         [MaxLength(100)]
         public string fullName { get; set; } // user input
+
 
         [Required(ErrorMessage = "Phone number is required.")]
         [MaxLength(15)]
         public string phoneNumber { get; set; } // user input
 
+
         [Required(ErrorMessage = "specialization is required.")]
         [MaxLength(100)]
-        public string specialization { get; set; } // Entered by the trainer
+        public string specialization { get; set; } // user input
+
 
         [Required(ErrorMessage = "yearsOfExperience is required.")]
         [Range(0, 50)]
         public int yearsOfExperience { get; set; } // user input
 
+
         [MaxLength(200)]
         public string? certification { get; set; } // user input
+
+
+        [Required]
+        [ForeignKey("User")]
+        public int userId { get; set; }   // Foreign Key Value comes from the registered user account.
+
 
         // Navigation Properties
         public User User { get; set; }// One User has one Trainer profile
