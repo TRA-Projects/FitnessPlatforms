9a4efa861482bd3e350d12ed501100d02cd56a8d
Aliya Alisaii <AliyaRashidSarhan@gmail.com>
2026-07-21 13:26:01 +0400

    add fitness context

--- FitnessPlatform/FitnessPlatform/FitnessContext.cs
+++ FitnessPlatform/FitnessPlatform/FitnessContext.cs
@@ -1,9 +1,22 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FitnessPlatform.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace FitnessPlatform
 {
     public class FitnessContext:DbContext
     {
         public FitnessContext(DbContextOptions<FitnessContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Trainer> Trainers { get; set; }
+        public DbSet<MembershipPlan> MembershipPlans { get; set; }
+        public DbSet<Subscription> Subscriptions { get; set; }
+        public DbSet<WorkoutProgram> WorkoutPrograms { get; set; }
+        public DbSet<Exercise> Exercises { get; set; }
+        public DbSet<ProgramExercise> ProgramExercises { get; set; }
+        public DbSet<WorkoutSession> WorkoutSessions { get; set; }
+        public DbSet<BodyMeasurement> BodyMeasurements { get; set; }
+        public DbSet<NutritionPlan> NutritionPlans { get; set; }
     }
 }
