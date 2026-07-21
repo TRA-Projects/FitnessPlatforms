f649d2ce9346d36a8bb2166c423e99fc73a75d7e
kareema <kareema27-19@gmail.com>
2026-07-21 14:49:31 +0400

    Add MembershipPlan DTOs

--- FitnessPlatform/FitnessPlatform/DTOs/MembershipPlanDTOs.cs
+++ FitnessPlatform/FitnessPlatform/DTOs/MembershipPlanDTOs.cs
@@ -1,6 +1,47 @@
-﻿namespace FitnessPlatform.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitnessPlatform.DTOs
 {
+    // =====================================
+    // Used when creating a new membership plan
+    // =====================================
     public class MembershipPlanDTOs
     {
+        [Required(ErrorMessage = "Plan name is required.")]
+        [MaxLength(50)]
+        public string planName { get; set; }   // Entered by the Admin
+
+
+        [Required(ErrorMessage = "Price is required.")]
+        [Range(1, 10000, ErrorMessage = "Price must be greater than 0.")]
+        public decimal price { get; set; }  // Entered by the Admin
+
+
+        [Required(ErrorMessage = "Duration is required.")]
+        [Range(1, 3650, ErrorMessage = "Duration must be greater than 0.")]
+        public int durationInDays { get; set; }   // Entered by the Admin
+
+
+        [MaxLength(500)]
+        public string? description { get; set; }   // Entered by the Admin
+
+
+
+        // =====================================
+        // Used when displaying all membership plans
+        // =====================================
+        public class MembershipPlanOutputDTO
+        {
+           
+            public int planId { get; set; } // Membership Plan ID
+
+            
+            public string planName { get; set; } // Membership Plan Name
+
+            
+            public decimal price { get; set; } // Membership Price
+        }
+
+
     }
 }
