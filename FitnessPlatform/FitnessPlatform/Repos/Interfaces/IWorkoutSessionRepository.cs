c6475073a6ee1e8fc49094f4d51dec74f652bbac
Tasnim ALGhafri <tasnimalghafri1@gmail.com>
2026-07-23 10:21:57 +0400

    Adding IWorkoutSessionRepository functions

--- FitnessPlatform/FitnessPlatform/Repos/Interfaces/IWorkoutSessionRepository.cs
+++ FitnessPlatform/FitnessPlatform/Repos/Interfaces/IWorkoutSessionRepository.cs
@@ -1,6 +1,26 @@
-﻿namespace FitnessPlatform.Repos.Interfaces
+﻿using FitnessPlatform.Models;
+
+namespace FitnessPlatform.Repos.Interfaces
 {
-    public class IWorkoutSessionRepository
+    public interface IWorkoutSessionRepository
     {
+        //Get all workout sessions
+        Task<IEnumerable<WorkoutSession>> GetAllWorkoutSessionsAsync();
+
+        // Get workout session by id
+        Task<WorkoutSession?> GetWorkoutSessionByIdAsync(int id);
+
+        // Get workout sessions by member id
+        Task<IEnumerable<WorkoutSession>> GetWorkoutSessionsByMemberIdAsync(int memberId);
+
+        // Create new workout session
+        Task<WorkoutSession> CreateWorkoutSessionAsync(WorkoutSession workoutSession);
+
+        // Update workout session
+        Task<WorkoutSession> UpdateWorkoutSessionAsync(WorkoutSession workoutSession);
+
+        // Delete workout session
+        Task DeleteWorkoutSessionAsync(int id);
+
     }
 }
