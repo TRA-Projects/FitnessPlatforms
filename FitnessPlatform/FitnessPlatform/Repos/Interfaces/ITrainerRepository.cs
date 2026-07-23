420617272f72ec7da113f5b0369f2e48459f70da
kareema <kareema27-19@gmail.com>
2026-07-23 10:37:45 +0400

    Add Trainer Interface

--- FitnessPlatform/FitnessPlatform/Repos/Interfaces/ITrainerRepository.cs
+++ FitnessPlatform/FitnessPlatform/Repos/Interfaces/ITrainerRepository.cs
@@ -1,6 +1,23 @@
-﻿namespace FitnessPlatform.Repos.Interfaces
+﻿using FitnessPlatform.Models;
+
+namespace FitnessPlatform.Repos.Interfaces
 {
-    public class ITrainerRepository
+    public interface ITrainerRepository
     {
+
+        // Get all trainers
+        Task<IEnumerable<Trainer>> GetAllTrainers();
+
+        // Get trainer by id
+        Task<Trainer?> GetTrainerById(int id);
+
+        // Create new trainer
+        Task<Trainer> CreateTrainer(Trainer trainer);
+
+        // Update trainer
+        Task UpdateTrainer(Trainer trainer);
+
+        // Delete trainer
+        Task DeleteTrainer(int id);
     }
 }
