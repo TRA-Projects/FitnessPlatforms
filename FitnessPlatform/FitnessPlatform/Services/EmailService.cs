cd7617de7049937d86df1488b7f854d290540d7e
Aliya Alisaii <AliyaRashidSarhan@gmail.com>
2026-07-28 09:38:09 +0400

    create EmailService

---
+++ FitnessPlatform/FitnessPlatform/Services/EmailService.cs
@@ -0,0 +1,43 @@
+﻿using FitnessPlatform.Configurations;
+using MailKit.Net.Smtp;
+using Microsoft.Extensions.Options;
+using MimeKit;
+
+namespace FitnessPlatform.Services
+{
+    public class EmailService
+    {
+        private readonly EmailSettings _emailSettings;
+
+        public EmailService(IOptions<EmailSettings> emailSettings)
+        {
+            _emailSettings = emailSettings.Value;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string subject, string body)
+        {
+            var email = new MimeMessage();
+
+            email.From.Add(MailboxAddress.Parse(_emailSettings.Email));
+
+            email.To.Add(MailboxAddress.Parse(toEmail));
+
+            email.Subject = subject;
+
+            email.Body = new TextPart("plain")
+            {
+                Text = body
+            };
+
+            using var smtp = new SmtpClient();
+
+            await smtp.ConnectAsync(_emailSettings.Host, _emailSettings.Port, MailKit.Security.SecureSocketOptions.StartTls);
+
+            await smtp.AuthenticateAsync(_emailSettings.Email, _emailSettings.Password);
+
+            await smtp.SendAsync(email);
+
+            await smtp.DisconnectAsync(true);
+        }
+    }
+}
